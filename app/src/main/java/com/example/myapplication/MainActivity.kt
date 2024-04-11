bd5a8298adc451fd112e7eb70f63fe07f179c2de
thomasvld <thomas.valade@viacesi.fr>
2024-04-11 14:44:34 +0200

    Création des boutons dans un composable Application Création de colonne pour lka mise en page Dû à des problèmes techniques independants de ma volonté ce projet ne verra pas le jour....

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +22,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ){
+                    Text("Bienvenue sur scan ou cannes")
+                    Text(text = "Choisissez quel manga vous souhaitez lire :")
+                    Application()
                 }
+
             }
         }
     }
@@ -32,15 +42,56 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = " $name!",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(  //Pour background+en-tete
+    showBackground = true,
+    showSystemUi = true,
+    name = "My Preview"
+)
+
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ){
+        Text("Bienvenue sur scan ou cannes")
+        Text(text = "Choisissez quel manga vous souhaitez lire")
+
+        Application()
+        }
+    }
+}
+
+
+@Composable
+fun Application(){
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Bleach")
+    }
+
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Dragon Ball")
+    }
+
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Naruto")
+    }
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "One Piece")
     }
 }
